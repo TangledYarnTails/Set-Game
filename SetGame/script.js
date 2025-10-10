3ca90fafbb9d5055745dc565074425921935c419
AnushkaGuptaW <tangled.yarn.tails@gmail.com>
2025-10-10 18:21:03 +0900

    filling out logic for dealing cards
    dealing cards function, full deck, selecting cards functions have been added

--- SetGame/script.js
+++ SetGame/script.js
@@ -1,31 +1,589 @@
 // 81 objects(cards)
 let deck =[
-  {
-    shape: "lay",
-    pattern: "solid",
-    color: "brown",
+   {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "red",
     number: 1,
-    name: "one"
-  }
+    name: "oneOutlineRedRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "red",
+    number: 2,
+    name: "twoOutlineRedRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "red",
+    number: 3,
+    name: "threeOutlineRedRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "red",
+    number: 1,
+    name: "oneStripedRedRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "red",
+    number: 2,
+    name: "twoStripedRedRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "red",
+    number: 3,
+    name: "threeStripedRedRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "red",
+    number: 1,
+    name: "oneSolidRedRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "red",
+    number: 2,
+    name: "twoSolidRedRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "red",
+    number: 3,
+    name: "threeSolidRedRhombus"
+  },
+    {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "green",
+    number: 1,
+    name: "oneOutlineGreenRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "green",
+    number: 2,
+    name: "twoOutlineGreenRhombus"
+  }, 
+  {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "green",
+    number: 3,
+    name: "threeOutlineGreenRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "green",
+    number: 1,
+    name: "oneStripedGreenRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "green",
+    number: 2,
+    name: "twoStripedGreenRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "green",
+    number: 3,
+    name: "threeStripedGreenRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "green",
+    number: 1,
+    name: "oneSolidGreenRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "green",
+    number: 2,
+    name: "twoSolidGreenRhombus"
+  }, {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "green",
+    number: 3,
+    name: "threeSolidGreenRhombus"
+  },
+   {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "blue",
+    number: 1,
+    name: "oneOutlineBlueRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "blue",
+    number: 2,
+    name: "twoOutlineBlueRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "outline",
+    color: "blue",
+    number: 3,
+    name: "threeOutlineBlueRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "blue",
+    number: 1,
+    name: "oneStripedBlueRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "blue",
+    number: 2,
+    name: "twoStripedBlueRhombus"
+  },
+  {
+    shape: "rhombus",
+    pattern: "striped",
+    color: "blue",
+    number: 3,
+    name: "threeStripedBlueRhombus"
+  },
+    {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "blue",
+    number: 1,
+    name: "oneSolidBlueRhombus"
+  },
+    {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "blue",
+    number: 2,
+    name: "twoSolidBlueRhombus"
+  },
+    {
+    shape: "rhombus",
+    pattern: "solid",
+    color: "blue",
+    number: 3,
+    name: "threeSolidBlueRhombus"
+  },
+  {
+    shape: "pill",
+    pattern: "outline",
+    color: "red",
+    number: 1,
+    name: "oneOutlineRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "outline",
+    color: "red",
+    number: 2,
+    name: "twoOutlineRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "outline",
+    color: "red",
+    number: 3,
+    name: "threeOutlineRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "striped",
+    color: "red",
+    number: 1,
+    name: "oneStripedRedPill"
+  }, {
+    shape: "pill",
+    pattern: "striped",
+    color: "red",
+    number: 2,
+    name: "twoStripedRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "striped",
+    color: "red",
+    number: 3,
+    name: "threeStripedRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "solid",
+    color: "red",
+    number: 1,
+    name: "oneSolidRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "solid",
+    color: "red",
+    number: 2,
+    name: "twoSolidRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "solid",
+    color: "red",
+    number: 3,
+    name: "threeSolidRedPill"
+  },
+   {
+    shape: "pill",
+    pattern: "outline",
+    color: "green",
+    number: 1,
+    name: "oneOutlineGreenPill"
+  },
+   {
+    shape: "pill",
+    pattern: "outline",
+    color: "green",
+    number: 2,
+    name: "twoOutlineGreenPill"
+  },
+   {
+    shape: "pill",
+    pattern: "outline",
+    color: "green",
+    number: 3,
+    name: "threeOutlineGreenPill"
+  },
+   {
+    shape: "pill",
+    pattern: "striped",
+    color: "green",
+    number: 1,
+    name: "oneStripedGreenPill"
+  },
+   {
+    shape: "pill",
+    pattern: "striped",
+    color: "green",
+    number: 2,
+    name: "twoStripedGreenPill"
+  },
+   {
+    shape: "pill",
+    pattern: "striped",
+    color: "green",
+    number: 3,
+    name: "threeStripedGreenPill"
+  },
+   {
+    shape: "pill",
+    pattern: "solid",
+    color: "green",
+    number: 1,
+    name: "oneSolidGreenPill"
+  },
+  {
+    shape: "pill",
+    pattern: "solid",
+    color: "green",
+    number: 2,
+    name: "twoSolidGreenPill"
+  },
+  {
+    shape: "pill",
+    pattern: "solid",
+    color: "green",
+    number: 3,
+    name: "threeSolidGreenPill"
+  },
+  {
+    shape: "pill",
+    pattern: "outline",
+    color: "blue",
+    number: 1,
+    name: "oneOutlineBluePill"
+  },
+  {
+    shape: "pill",
+    pattern: "outline",
+    color: "blue",
+    number: 2,
+    name: "twoOutlineBluePill"
+  },
+  {
+    shape: "pill",
+    pattern: "outline",
+    color: "blue",
+    number: 3,
+    name: "threeOutlineBluePill"
+  },
+  {
+    shape: "pill",
+    pattern: "striped",
+    color: "blue",
+    number: 1,
+    name: "oneStripedBluePill"
+  },
+   {
+    shape: "pill",
+    pattern: "striped",
+    color: "blue",
+    number: 2,
+    name: "twoStripedBluePill"
+  },
+   {
+    shape: "pill",
+    pattern: "striped",
+    color: "blue",
+    number: 3,
+    name: "threeStripedBluePill"
+  },
+   {
+    shape: "pill",
+    pattern: "solid",
+    color: "blue",
+    number: 1,
+    name: "oneSolidBluePill"
+  },
+   {
+    shape: "pill",
+    pattern: "solid",
+    color: "blue",
+    number: 2,
+    name: "twoSolidBluePill"
+  }, {
+    shape: "pill",
+    pattern: "solid",
+    color: "blue",
+    number: 3,
+    name: "threeSolidBluePill"
+  },
+   {
+    shape: "chevron",
+    pattern: "outline",
+    color: "red",
+    number: 1,
+    name: "oneOutlineRedChevron"
+  },
+    {
+    shape: "chevron",
+    pattern: "outline",
+    color: "red",
+    number: 2,
+    name: "twoOutlineRedChevron"
+  },
+    {
+    shape: "chevron",
+    pattern: "outline",
+    color: "red",
+    number: 3,
+    name: "threeOutlineRedChevron"
+  },
+    {
+    shape: "chevron",
+    pattern: "striped",
+    color: "red",
+    number: 1,
+    name: "oneStripedRedChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "striped",
+    color: "red",
+    number: 2,
+    name: "twoStripedRedChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "striped",
+    color: "red",
+    number: 3,
+    name: "threeStripedRedChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "solid",
+    color: "red",
+    number: 1,
+    name: "oneSolidRedChevron"
+  },
+     {
+    shape: "chevron",
+    pattern: "solid",
+    color: "red",
+    number: 2,
+    name: "twoSolidRedChevron"
+  },
+     {
+    shape: "chevron",
+    pattern: "solid",
+    color: "red",
+    number: 3,
+    name: "threeSolidRedChevron"
+  },
+     {
+    shape: "chevron",
+    pattern: "outline",
+    color: "green",
+    number: 1,
+    name: "oneOutlineGreenChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "outline",
+    color: "green",
+    number: 2,
+    name: "twoOutlineGreenChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "outline",
+    color: "green",
+    number: 3,
+    name: "threeOutlineGreenChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "striped",
+    color: "green",
+    number: 1,
+    name: "oneStripedGreenChevron"
+  },
+  {
+    shape: "chevron",
+    pattern: "striped",
+    color: "green",
+    number: 2,
+    name: "twoStripedGreenChevron"
+  },{
+    shape: "chevron",
+    pattern: "striped",
+    color: "green",
+    number: 3,
+    name: "threeStripedGreenChevron"
+  },
+  {
+    shape: "chevron",
+    pattern: "solid",
+    color: "green",
+    number: 1,
+    name: "oneSolidGreenChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "solid",
+    color: "green",
+    number: 2,
+    name: "twoSolidGreenChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "solid",
+    color: "green",
+    number: 3,
+    name: "threeSolidGreenChevron"
+  },
+  {
+    shape: "chevron",
+    pattern: "outline",
+    color: "blue",
+    number: 1,
+    name: "oneOutlineBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "outline",
+    color: "blue",
+    number: 2,
+    name: "twoOutlineBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "outline",
+    color: "blue",
+    number: 3,
+    name: "threeOutlineBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "striped",
+    color: "blue",
+    number: 1,
+    name: "oneStripedBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "striped",
+    color: "blue",
+    number: 2,
+    name: "twoStripedBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "striped",
+    color: "blue",
+    number: 3,
+    name: "threeStripedBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "solid",
+    color: "blue",
+    number: 1,
+    name: "oneSolidBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "solid",
+    color: "blue",
+    number: 2,
+    name: "twoSolidBlueChevron"
+  },
+   {
+    shape: "chevron",
+    pattern: "solid",
+    color: "blue",
+    number: 3,
+    name: "threeSolidBlueChevron"
+  },
 ];
 let selectedCards =[] //3 cards that were clicked on
 let board = []; //12 or 15 cards on the board (3x4 grid)
 let takenCards =[]; // cards that were verified that are no longer playable
 let points; // player's points, 1 set = 1 point, 1 wrong set = -1point
 let initial = true; // initial deal is 12 cards, otherwise just 3
+const cardNodeList = document.querySelectorAll(".card"); //get all card elements
 
-function dealCards(){
+
+function dealCards(){ //takes cards from deck and adds them to board array
 let x = 0;
     if(initial == true){
 //take a random index from the deck 12 times and add it to the board array
 let chosen;
-while(x > 12){
-    chosen = (int)(Math.random() * deck.length )
-board.push(deck[chosen]) //random index values from 0 to 81
+while(x < 12){
+    chosen = Math.floor((Math.random() * deck.length ))
+console.log(chosen)
+    board.push(deck[chosen]) //random index values from 0 to 81
 deck.splice(chosen, 1) // removes one random object from the deck
 initial = false;
-console.log("Currently on the board: " + board)
-console.log("Deck Length: " + deck.length)
+assignBoard();
 x++;
 }
 
@@ -40,20 +598,46 @@ console.log("Deck Length: " + deck.length)
 x++;
 }
 }
-};
 
-function setsInBoard(){
-// is there at least one possible set on the board?
-if(){
-
-}else{
-    dealCards();
-}
-// if no then deal cards
 };
-function getCard(card) { // returns if the card selected is on the board
-return board.includes(Card.name);
+function assignBoard(){
+   for(let i=0 ; i < board.length ; i++){
+    let x=0;
+    let childArray =  Array.from(cardNodeList[i].children); // one two three
+    while (x < board[i].number){
+       childArray[x].style.display ="flex";
+       childArray[x].classList.add(board[i].shape);
+
+       if(board[i].pattern == "striped"){
+       childArray[x].style.background = `repeating-linear-gradient(0deg, var(--${board[i].color}), var(--${board[i].color}) 10px, #ffffff 10px, #ffffff 20px)`;
+    
+       } else if(board[i].pattern == "outline"){
+        childArray[x].style.border = `5px solid ${board[i].color}`
+         childArray[x].style.backgroundColor = "#ffffff";
+       }else{
+        childArray[x].style.backgroundColor = `var(--${board[i].color})`;
+       }
+       
+       console.log(board[i].number);
+       console.log(childArray[x]); 
+x++;
+    }
+    x= 0;
+}   
+};
+//adds the clicked on cards to the array of selected cards
+function addToSelectedCardsArr(){
+  cardNodeList.forEach(item => {
+  item.addEventListener('click', () => {
+    if(selectedCards.length < 3 && !selectedCards.includes(item)){ 
+      selectedCards.push(item);
+      item.style.border = "2px solid blue";
+    }
+     console.log(selectedCards);
+  });
+});
 }
+
 function verifySet(){
 //compare all three cards to each other
 // consider each characteristic one at a time
@@ -88,3 +672,20 @@ if (
 }
 
 };
+//is there at least one set in the board?
+function setsInBoard(){
+// is there at least one possible set on the board?
+
+
+
+// if no then deal cards
+};
+
+//function calls-----------------------------------------------
+dealCards();
+addToSelectedCardsArr();
+verifySet()
+//console.log(deck);
+
+console.log(board);
+//console.log ("card node list length " +cardNodeList.length)
